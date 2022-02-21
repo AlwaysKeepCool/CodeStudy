3232857109eb94aa3e53c35c24b389b2ebb07d8d
aaaaaj-create <1034737691@qq.com>
2022-02-21 23:04:28 +0800

    second commit

---
+++ 20220219-2/20220219/test.c
@@ -0,0 +1,51 @@
+#include "SeqList.h"
+
+//测试
+void TestSeqList1()
+{
+	SeqList s;
+	SeqListInit(&s);
+	//初始化
+	SeqListPushBack(&s, 1);
+	SeqListPushBack(&s, 2);
+	SeqListPushBack(&s, 3);
+	SeqListPushBack(&s, 4);
+	SeqListPushBack(&s, 5);
+	SeqListPrint(&s);
+
+	seqListPopBack(&s);
+	seqListPopBack(&s);
+	SeqListPrint(&s);//虽然这里传结构体也行，但会压栈
+
+	SeqListPushFront(&s, 0);
+	SeqListPushFront(&s, 0);
+	SeqListPushFront(&s, 0);
+	SeqListPrint(&s);
+
+	SeqListInsert(&s, 2, 520);
+	SeqListPrint(&s);
+
+	SeqListErase(&s, 2);
+	SeqListPrint(&s);
+
+	int pos=SeqListSearch(&s, 2);
+	if (pos != -1)
+	{
+		SeqListErase(&s, pos);
+	}
+	SeqListPrint(&s);
+
+	SeqListDestory(&s);
+
+
+}
+
+
+
+int  main()
+{
+
+	TestSeqList1();
+
+	return 0;
+}
