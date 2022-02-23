752a47432a8aeff08727cf10bee0e99fac49506e
aaaaaj-create <1034737691@qq.com>
2022-02-23 11:22:33 +0800

    second commit

---
+++ List2/List/SList.h
@@ -0,0 +1,22 @@
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+
+typedef int SListDataType;
+//s=single  单链表节点的定义
+typedef struct SListNode
+{
+	SListDataType data;   //数据域
+	struct SListNode* next;  //指针域
+}SListNode;
+
+SListNode* SListCreateNode(SListDataType n);
+void SListPushBack(SListNode** pphead, SListDataType n);
+void SListPopBack(SListNode** pphead);
+void SListPushFront(SListNode** pphead, SListDataType n);
+void SListPopFront(SListNode** pphead);
+void SListPrint(SListNode* phead);
+SListNode* SListSearch(SListNode* phead,SListDataType n);
+void SListInsertAfter(SListNode* pos, SListDataType n);
+void SListEraseAfter(SListNode* pos);
---
+++ List2/List/test.c
@@ -0,0 +1,83 @@
+#include"SList.h"
+
+void TestSList()
+{
+	//SListNode phead;//直接定义一个头节点？
+	//此时只需要一个指针即可，不需要结点！！！！
+	SListNode* pList = NULL;//因为头指针只有指针域没有数据域，这里只需要一个指针变量即可
+
+	SListPrint(pList);
+	SListPushBack(&pList, 1);
+	SListPushBack(&pList, 1);
+	SListPushBack(&pList, 1);
+	SListPushBack(&pList, 1);
+	SListPrint(pList);
+
+	SListPopBack(&pList);
+	SListPopBack(&pList);
+	SListPopBack(&pList);
+	SListPopBack(&pList);
+	SListPopBack(&pList);
+	SListPrint(pList);
+
+	SListPushFront(&pList, 1);
+	SListPushFront(&pList, 2);
+	SListPushFront(&pList, 3);
+	SListPushFront(&pList, 4);
+	SListPushFront(&pList, 5);
+	//头插打印过来的值是颠倒过来的
+	SListPrint(pList);
+
+	SListPopFront(&pList);
+	SListPopFront(&pList);
+	SListPopFront(&pList);
+	SListPopFront(&pList);
+	SListPopFront(&pList);
+	SListPrint(pList);
+	SListPopFront(&pList);
+
+
+	SListPushFront(&pList, 1);
+	SListPushFront(&pList, 2);
+	SListPushFront(&pList, 3);
+	SListPushFront(&pList, 4);
+	SListPrint(pList);
+
+	SListNode* pos=SListSearch(pList, 2);
+	if (pos!= NULL)
+	{
+		pos->data = 666;
+	}
+	SListPrint(pList);
+
+
+
+	
+}
+
+void TestSList2()
+{
+	SListNode* pList=NULL;
+	SListPushBack(&pList, 1);
+	SListPushBack(&pList, 2);
+	SListPushBack(&pList, 3);
+	SListPushBack(&pList, 4);
+	SListPrint(pList);
+	
+	SListNode* pos=SListSearch(pList,1);
+	SListInsertAfter(pos, 666);
+	SListPrint(pList);
+
+	pos= SListSearch(pList, 2);
+	SListEraseAfter(pos);
+	SListPrint(pList);
+
+
+}
+
+
+int main()
+{
+	TestSList2();
+	return 0;
+}
